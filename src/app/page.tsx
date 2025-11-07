4db046eb896155eed336d25785a93f7550d33d9c
Shinia-Gupta <shiniavit@gmail.com>
2025-11-07 12:32:21 +0530

    implement signin with github, signout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+"use client"
+
+import { login } from "@/src/lib/actions/auth";
+
 
 export default function Home() {
+  
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      HI
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h2>You are not signed in-</h2>
+      <button onClick={()=>login()}>Sign in with Github</button>
     </div>
   );
 }
