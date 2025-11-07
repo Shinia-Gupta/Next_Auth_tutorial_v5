c6e29e34da4a2a62f99f51fd9b5fb67b18373619
Shinia-Gupta <shiniavit@gmail.com>
2025-11-07 12:49:24 +0530

    accessing session data and integrate image on page.tsx

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env:{
+
+  },
+  images:{
+    domains:["avatars.githubusercontent.com"]
+  }
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,32 @@
-"use client"
+"use server"
 
-import { login } from "@/src/lib/actions/auth";
+import { auth } from "@/src/auth";
+import SigninBtn from "@/src/app/components/sign-in-btn";
+import Image from "next/image";
+import SignoutBtn from "@/src/app/components/sign-out-button";
 
 
-export default function Home() {
-  
+export default async function Home() {
+  const session = await auth();
+  console.log("session- ", session);
+
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h2>You are not signed in-</h2>
-      <button onClick={()=>login()}>Sign in with Github</button>
+      {!session ?
+        <>
+          <h2>You are not signed in-</h2>
+          <SigninBtn />
+        </>
+        :
+        <>
+          <h2> Next Auth v5 + Next 16</h2>
+          <p>User signed in with name: {session?.user?.name}</p>
+          <p>User signed in with email: {session?.user?.email}</p>
+          {session?.user?.image &&
+            <Image src={session?.user?.image} width={48} height={48} alt={session?.user.name || "Avatar"} />}
+          <SignoutBtn />
+        </>
+      }
     </div>
   );
 }
